158266ccb6d609374c77cd059232ae63caafa93c
Kaloyan Preslavski <kalopresli@gmail.com>
2024-04-21 21:53:31 +0200

    Changed DB to Cassandra. Updated dependencies. Put it in Docker-Compose.

--- src/main/java/com/backend/votingservice/repository/PartyRepository.java
+++ src/main/java/com/backend/votingservice/repository/PartyRepository.java
@@ -1,15 +1,17 @@
 package com.backend.votingservice.repository;
 
 import com.backend.votingservice.domain.Party;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.util.List;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
 import java.util.Optional;
 
-public interface PartyRepository  extends JpaRepository<Party, Long> {
-    @Query("SELECT p FROM Party p JOIN p.memberIds memberId WHERE memberId = :userId")
-    Optional<Party> findPartyByMemberId(@Param("userId") Long userId);
+public interface PartyRepository  extends CassandraRepository<Party, Long> {
+
+    // WARNING: Using ALLOW FILTERING is generally not recommended for production systems as it can lead to severe performance issues due to full table scans, especially as data grows. This is a temporary solution to enable functionality but should be revisited.
+    /* TODO: Reconsider the data model for the 'member_ids' usage. Ideally, restructure the database schema to avoid the need for data filtering in queries.
+             This might involve denormalizing data or creating additional tables specifically structured to support efficient queries for membership checks.
+             Consult data modeling best practices for Cassandra and consider re-designing the schema to improve query performance and scalability.*/
+    @Query("SELECT * FROM party WHERE member_ids contains :userId ALLOW FILTERING")
+    Optional<Party> findPartyByMemberId(Long userId);
 
 }
--- src/main/java/com/backend/votingservice/service/PartyService.java
+++ src/main/java/com/backend/votingservice/service/PartyService.java
@@ -3,7 +3,6 @@ package com.backend.votingservice.service;
 import com.backend.votingservice.domain.Party;
 import com.backend.votingservice.dto.UserFavoriteMoviesMessage;
 import com.backend.votingservice.repository.PartyRepository;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,14 +13,14 @@ public class PartyService {
     private final PartyRepository partyRepository;
 
 
-    @Transactional
+
     public void processFavoriteMoviesMessage(UserFavoriteMoviesMessage message) {
         Party party = findOrCreatePartyForUser(message.getUserId());
         party.getMovieExternalIds().addAll(message.getFavoriteMovieIds());
         partyRepository.save(party);
     }
 
-    @Transactional
+
     private Party findOrCreatePartyForUser(Long userId) {
         return partyRepository.findPartyByMemberId(userId)
                 .orElseGet(() -> {
@@ -32,7 +31,7 @@ public class PartyService {
                 });
     }
 
-    @Transactional
+
     public Party getParty(Long userId) throws Exception {
         return partyRepository.findPartyByMemberId(userId).orElseThrow(() -> new Exception("User not found - " + userId));
     }
